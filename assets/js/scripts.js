16dca2e7998956e395f3d7a1f9980b4923be88f0
Udesh Kumarasinghe <mail@udesh.xyz>
2019-08-28 03:35:23 +0530

    Created initial timeline

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,12 @@
+// Scroll reveals
 ScrollReveal().reveal('.header-logo', { delay: 1000, reset: true });
 ScrollReveal().reveal('.countdown', { delay: 1500, reset: true });
 ScrollReveal().reveal('.header-register', { delay: 1500, reset: true });
 ScrollReveal().reveal('.header-scroll', { delay: 2000, reset: true });
 
+ScrollReveal().reveal('.item', { delay: 500, reset: true });
+
+// Countdown timer
 var timerId =
     countdown(
         new Date("2019-10-12"),
@@ -12,3 +16,7 @@ var timerId =
             document.getElementById('hours').innerHTML = ts.hours;
         },
         countdown.DAYS | countdown.HOURS);
+
+// Rellax
+var rellax = new Rellax('.rellax');
+var rellax = new Rellax('.floating-item');
