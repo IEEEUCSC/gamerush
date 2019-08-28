ef88737619687af782a38ae147168135f74fdc86
Udesh Kumarasinghe <mail@udesh.xyz>
2019-08-28 05:51:34 +0530

    Created sponsors section

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,7 +4,13 @@ ScrollReveal().reveal('.countdown', { delay: 1500, reset: true });
 ScrollReveal().reveal('.header-register', { delay: 1500, reset: true });
 ScrollReveal().reveal('.header-scroll', { delay: 2000, reset: true });
 
+ScrollReveal().reveal('.timeline-title', { delay: 500, reset: true });
+ScrollReveal().reveal('.prizes-title', { delay: 500, reset: true });
+ScrollReveal().reveal('.sponsors-title', { delay: 500, reset: true });
+
 ScrollReveal().reveal('.item', { delay: 500, reset: true });
+ScrollReveal().reveal('.prizes-medal', { delay: 800, reset: true });
+ScrollReveal().reveal('.sponsor-item', { delay: 800, reset: true });
 
 // Countdown timer
 var timerId =
