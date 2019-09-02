2415f21bba4feb959dcbf3d37a7af8ba9232c625
Udesh Kumarasinghe <mail@udesh.xyz>
2019-09-02 11:59:55 +0530

    Add upnext and footer

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,18 +4,18 @@ ScrollReveal().reveal('.countdown', { delay: 1500, reset: true });
 ScrollReveal().reveal('.header-register', { delay: 1500, reset: true });
 ScrollReveal().reveal('.header-scroll', { delay: 2000, reset: true });
 
-ScrollReveal().reveal('.timeline-title', { delay: 500, reset: true });
-ScrollReveal().reveal('.prizes-title', { delay: 500, reset: true });
-ScrollReveal().reveal('.sponsors-title', { delay: 500, reset: true });
+// ScrollReveal().reveal('.timeline-title', { delay: 500, reset: true });
+// ScrollReveal().reveal('.prizes-title', { delay: 500, reset: true });
+// ScrollReveal().reveal('.sponsors-title', { delay: 500 });
 
 ScrollReveal().reveal('.item', { delay: 500, reset: true });
 ScrollReveal().reveal('.prizes-medal', { delay: 800, reset: true });
-ScrollReveal().reveal('.sponsor-item', { delay: 800, reset: true });
+ScrollReveal().reveal('.sponsor-section', { delay: 800, reset: true });
 
 // Countdown timer
 var timerId =
     countdown(
-        new Date("2019-10-12"),
+        new Date("2019-09-17"),
         function (ts) {
             document.getElementById('days').innerHTML = ts.days;
             document.getElementById('hours').innerHTML = ts.hours;
