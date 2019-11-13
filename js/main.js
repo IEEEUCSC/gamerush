0a14e8287bb533398ca2a50c072cd71977a3cc23
Udesh Kumarasinghe <mail@udesh.xyz>
2019-11-13 17:22:38 +0000

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var baseUrl = 'http://gamerush.ucscieee.com/';
+
+if (location.href == baseUrl + 'register') {
+    window.location = 'https://docs.google.com/forms/d/e/1FAIpQLScJnzNVYKg53uUpwPDdVZSFtWXoA9EGIHTqqwVO2NYuTwv9Fg/viewform?usp=sf_link';
+}
