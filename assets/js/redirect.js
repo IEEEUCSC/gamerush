1ddae29299210521c51c8cbd5c2000e20512e1ad
Udesh Kumarasinghe <mail@udesh.xyz>
2019-11-15 08:17:45 +0530

    Fixed redirect

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -1,5 +1,6 @@
 var baseUrl = 'http://gamerush.ucscieee.com/';
+var baseUrlSsl = 'https://gamerush.ucscieee.com/';
 
-if (location.href == baseUrl + 'register') {
+if (location.href == baseUrl + 'register' || location.href == baseUrlSsl + 'register') {
     window.location = 'https://docs.google.com/forms/d/e/1FAIpQLScJnzNVYKg53uUpwPDdVZSFtWXoA9EGIHTqqwVO2NYuTwv9Fg/viewform?usp=sf_link';
 }
